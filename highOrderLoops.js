ac7e294ed3583f77d0ec3fb7bedf917948e7d0ec
Sumit Kumar Jha <jhasumit4742@gmail.com>
2025-11-11 19:29:01 +0530

    Day03/25 challenge done

---
+++ highOrderLoops.js
@@ -0,0 +1,18 @@
+//for of
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const ele of arr) {
+  console.log(ele);
+}
+
+const greeting = "hello world";
+const charArr = [];
+for (let char of greeting) {
+  charArr.push(char);
+}
+console.log(charArr);
+
+// Maps
+
+const map = new Map();
